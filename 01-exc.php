0517684edfa877b5c9a13ec1fe48ba3b13d75776
alanssmeira <alan.sobreirasa@gmail.com>
2022-04-26 16:01:40 -0300

    conclusão ecx.

---
+++ 01-exc.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício PHP</title>
+</head>
+<body>
+    
+    <h1>Exercício PHP</h1>
+    <hr>
+
+    <?php
+
+    // Variáveis:
+    $hoje = date('d/m/Y');
+    $idade = 27;
+
+    // Contantes:
+    const MEU_NOME = "Alan Sobreira";
+    const MINHA_CIDADE = "São Paulo";
+
+    ?>
+    <p> Hoje é <?=$hoje?>. <?=MEU_NOME?> tem <?=$idade?> anos e nasceu em <?=MINHA_CIDADE?>. </p>
+
+</body>
+</html>
