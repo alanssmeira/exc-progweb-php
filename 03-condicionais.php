7da531d335fb503ec3fae60cb307ab18b2e4961f
alanssmeira <alan.sobreirasa@gmail.com>
2022-04-27 16:36:34 -0300

    exercício solução tentativa 1

---
+++ 03-condicionais.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 03 - Condicionais</title>
+</head>
+
+<body>
+
+    <h1>Exercício 03 - Condicionais</h1>
+    <hr>
+
+    <?php
+    $salario1 = 900;
+
+
+    $salario2 = $salario1 * 1.15;
+    $salario3 = $salario1 * 1.10;
+    $salario4 = $salario1 * 1.05;
+
+    if ($salario1 < 500) {
+        echo "<p> O salário é de $salario1, portanto o aumento é de 15%, resultando em  R$ $salario2.";
+    } elseif ($salario1 <= 1000) {
+        echo "<p> O salário é de $salario1, portanto o aumento é de 10%, resultando em  R$ $salario3.";
+    } else {
+        echo "<p> O salário é de $salario1, portanto o aumento é de 5%, resultando em R$ $salario4.";
+    }
+    
+    ?>
+
+</body>
+
+</html>
